fdb8c47370799eee9662dd9a3a2e8f56620e9009
jihannrhlz <jihannurhaliza26@gmail.com>
2023-10-26 14:59:40 +0700

    update index.html

---
+++ main.js
@@ -0,0 +1,9 @@
+function init(){
+    let tmbl_masuk = document.querySelector('#tmbl_masuk')
+
+    tmbl_masuk.addEventListener('click',()=>{
+    alert('Selamat ');
+    });
+}
+
+window.addEventListener('load', init());
